188f73adc8c56b03090670d5b0a3d25d8c4699eb
Jonathan Porter <mail@j.porter.name>
2025-03-27 17:15:06 -0700

    Refactor

--- HomeAssistant.ProcessNetworkKeyFromTlv/Program.cs
+++ HomeAssistant.ProcessNetworkKeyFromTlv/Program.cs
@@ -3,67 +3,22 @@ using HomeAssistant.ProcessNetworkKeyFromTlv;
 
 var data = new ReadOnlySpan<byte>(Convert.FromHexString(args[0]));
 
-foreach (var tlv in Tlv.ParseTlvs(data.ToArray()))
+foreach (var tlv in Tlv.ParseTlvs(data.ToArray()).OrderBy(tlv => tlv.Type))
 {
-    var tag = (MeshcopTlvType)tlv.Type.GetValueOrDefault();
+    var tag = (MeshCoPTlvType)tlv.Type.GetValueOrDefault();
     var value = new ReadOnlySpan<byte>(tlv.Value);
-    Console.WriteLine($"t: {(byte)tag,3} ({tag}), l: {tlv.Value.Length}, v: {FormatValue(in tag, in value)}");
+    Console.WriteLine($"t: {(byte)tag,-3} {$"({tag})",-29} l: {tlv.Value.Length,-5} v: {FormatValue(in tag, in value)}");
 }
 
 return;
 
-static string FormatValue(ref readonly MeshcopTlvType tag, ref readonly ReadOnlySpan<byte> value)
+static string FormatValue(ref readonly MeshCoPTlvType tag, ref readonly ReadOnlySpan<byte> value)
 {
     switch (tag)
     {
-        case MeshcopTlvType.NetworkName:
+        case MeshCoPTlvType.NetworkName:
             return Encoding.UTF8.GetString(value);
         default:
             return $"0x{Convert.ToHexString(value)}" ;
     }
 }
-
-
-enum MeshcopTlvType:byte {
-    Channel= 0,
-    PanID = 1,
-    ExtPanId = 2,
-    NetworkName = 3,
-    Pskc = 4,
-    NetworkKey = 5,
-    NetworkKeySequence = 6,
-    MeshLocalPrefix = 7,
-    SteeringData = 8,
-    BorderAgentRloc = 9,
-    CommissionerId = 10,
-    CommSessionId = 11,
-    SecurityPolicy = 12,
-    Get = 13,
-    ActiveTimestamp = 14,
-    CommissionerUdpPort = 15,
-    State = 16,
-    JoinerDtls = 17,
-    JoinerUdpPort = 18,
-    JoinerIID = 19,
-    JoinerRloc = 20,
-    JoinerRouterKek = 21,
-    ProvisioningUrl = 32,
-    VendorNameTlv = 33,
-    VendorModelTlv = 34,
-    VendorSwVersionTlv = 35,
-    VendorDataTlv = 36,
-    VendorStackVersionTlv = 37,
-    UdpEncapsulationTlv = 48,
-    Ipv6AddressTlv = 49,
-    PendingTimestamp = 51,
-    DelayTimer = 52,
-    ChannelMask = 53,
-    Count = 54,
-    Period = 55,
-    ScanDuration = 56,
-    EnergyList = 57,
-    DiscoveryRequest = 128,
-    DiscoveryResponse = 129,
-    JoinerAdvertisement = 241,
-}
-
--- HomeAssistant.ProcessNetworkKeyFromTlv/Tlv.cs
+++ HomeAssistant.ProcessNetworkKeyFromTlv/Tlv.cs
@@ -1,81 +1,78 @@
 ﻿using System.Buffers.Binary;
 using System.Text;
 
-namespace HomeAssistant.ProcessNetworkKeyFromTlv
+namespace HomeAssistant.ProcessNetworkKeyFromTlv;
+
+public class Tlv(int? type = null, byte[]? value = null)
 {
-    public class Tlv(int? type = null, byte[]? value = null)
+    public int? Type { get; set; } = type;
+    public byte[]? Value { get; set; } = value;
+
+    public override string ToString() => $"TLV\n\tTYPE:\t0x{Type:x2}\n\tVALUE:\t{AsString(Value)}";
+
+    private static string AsString(byte[]? bytes)
     {
-        public int? Type { get; set; } = type;
-        public byte[]? Value { get; set; } = value;
+        if (bytes is null)
+            return string.Empty;
 
-        public override string ToString() => $"TLV\n\tTYPE:\t0x{Type:x2}\n\tVALUE:\t{AsString(Value)}";
-
-        private static string AsString(byte[] bytes)
+        var sb = new StringBuilder(bytes.Length * 4 + 3);
+        sb.Append("b'");
+        foreach (var b in bytes)
         {
-            var sb = new StringBuilder(bytes.Length * 4 + 3);
-            sb.Append("b'");
-            foreach (var b in bytes)
-            {
-                sb.Append("\\x");
-                sb.Append(b.ToString("X2"));
-            }
-
-            sb.Append('\'');
-
-            return sb.ToString();
+            sb.Append("\\x");
+            sb.Append(b.ToString("X2"));
         }
 
-        public static IEnumerable<Tlv> ParseTlvs(byte[] data)
+        sb.Append('\'');
+
+        return sb.ToString();
+    }
+
+    public static IEnumerable<Tlv> ParseTlvs(byte[] data)
+    {
+        while (data.Length > 0)
         {
-            while (data.Length > 0)
-            {
-                var nextTlv = FromBytes(data);
-                var nextTlvSize = nextTlv.ToBytes().Length;
-                data = data[nextTlvSize..];
-                yield return nextTlv;
-            }
+            var nextTlv = FromBytes(data);
+            var nextTlvSize = nextTlv.ToBytes().Length;
+            data = data[nextTlvSize..];
+            yield return nextTlv;
         }
+    }
 
-        public static Tlv FromBytes(byte[] data)
+    public static Tlv FromBytes(byte[] data)
+    {
+        var result = new Tlv();
+        result.SetFromBytes(data);
+        return result;
+    }
+
+    public void SetFromBytes(byte[] data)
+    {
+        Type = data[0];
+        var range = data[1] == 0xFF
+            ? new Range(4, 4 + BinaryPrimitives.ReadUInt16BigEndian(data.AsSpan(2, 2)))
+            : new Range(2, 2 + data[1]);
+        Value = data[range];
+    }
+
+    public byte[] ToBytes()
+    {
+        var headerLength = Value.Length >= 254 ? 4 : 2;
+        var bytes = new byte[headerLength + Value.Length];
+        bytes[0] = (byte)Type.Value;
+        switch (headerLength)
         {
-            var result = new Tlv();
-            result.SetFromBytes(data);
-            return result;
-        }
-
-        public void SetFromBytes(byte[] data)
-        {
-            Type = data[0];
-            if (data[1] == 0xFF)
-            {
-                var length = BinaryPrimitives.ReadUInt16BigEndian(data.AsSpan(2, 2));
-                Value = data[4..(4 + length)];
-            }
-            else
-            {
-                Value = data[2..(2 + data[1])];
-            }
-        }
-
-        public byte[] ToBytes()
-        {
-            var headerLength = Value.Length >= 254 ? 4 : 2;
-
-            var bytes = new byte[headerLength + Value.Length];
-            bytes[0] = (byte)(Type.Value);
-            if (headerLength == 4)
-            {
+            case 2:
+                bytes[1] = (byte)Value.Length;
+                break;
+            case 4:
                 bytes[1] = 0xFF;
                 BinaryPrimitives.WriteUInt16BigEndian(bytes.AsSpan(2, 2), (ushort)Value.Length);
-            }
-            else
-            {
-                bytes[1] = (byte)Value.Length;
-            }
-
-            Value.CopyTo(bytes, headerLength);
-
-            return bytes;
+                break;
         }
+
+        Value.CopyTo(bytes, headerLength);
+
+        return bytes;
     }
 }
